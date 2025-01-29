72d6014e12e3990b1515ce267935f24f6a241762
Itssaravanakrish <77931259+Itssaravanakrish@users.noreply.github.com>
2025-01-29 17:40:04 +0530

    Update cmnds.py

--- plugins/cmnds.py
+++ plugins/cmnds.py
@@ -1,3 +1,6 @@
+Here is the complete code with the enums:
+
+
 from pyrogram import filters, Client
 from pyrogram.types import (
     InlineKeyboardMarkup,
@@ -5,12 +8,13 @@ from pyrogram.types import (
     Message,
     CallbackQuery,
 )
-
+from pyrogram.enums import ChatType
 from helpers.game import (
     get_game,
     new_game,
     next_word,
     is_true,
+    end_game,  # Import the end_game function
 )
 from helpers.wrappers import nice_errors
 from mongo import users, chats
@@ -21,14 +25,12 @@ inline_keyboard_markup = InlineKeyboardMarkup(
     [
         [
             InlineKeyboardButton(
-                'View',
-                callback_data='view',
+                "View", callback_data="view",
             ),
         ],
         [
             InlineKeyboardButton(
-                'Next',
-                callback_data='next',
+                "Next", callback_data="next",
             ),
         ],
     ]
@@ -37,71 +39,67 @@ inline_keyboard_markup = InlineKeyboardMarkup(
 @Client.on_message(filters.command("scores"))
 @nice_errors
 async def scores_callback(_, message: Message):
-    """ Handle the '/scores' command. Send the user's total scores and scores in the current chat. """
+    """Handle the '/scores' command. Send the user's total scores and scores in the current chat."""
     user_id = message.from_user.id
     chat_id = message.chat.id
     total_user_scores = await users.total_scores(user_id)
     scores_in_current_chat = (
         await users.scores_in_chat(chat_id, user_id)
-        if message.chat.type == 'supergroup'
-        else '<code>not in group</code>'
+        if message.chat.type == ChatType.SUPERGROUP
+        else "<code>not in group</code>"
     )
     await message.reply_text(
-        f'Your total scores: {total_user_scores}\nYour scores in this chat: {scores_in_current_chat}',
-        parse_mode="HTML"
+        f"Your total scores: {total_user_scores}\nYour scores in this chat: {scores_in_current_chat}",
+        parse_mode="HTML",
     )
 
-@Client.on_message(filters.command("start") & filters.chat_type.filters.GROUP)
+@Client.on_message(filters.command("start") & filters.chat_type(ChatType.GROUP))
 @nice_errors
 async def start_callback(_, message: Message):
-    """ Handle the '/start' command in a group chat. Start a new game and update the database with the chat details. """
+    """Handle the '/start' command in a group chat. Start a new game and update the database with the chat details."""
     await new_game(message.from_user)
-    
-    # Add try-except block for database update
     try:
         await chats.update(message.chat.id, message.chat.title)
     except Exception as e:
-        print(f"Error updating database: {e}")
-    
+        logging.error(f"Error updating database: {e}")
     await message.reply_text(
-        f'{message.from_user.mention_html()} talks about a word.',
+        f"{message.from_user.mention_html()} talks about a word.",
         reply_markup=inline_keyboard_markup,
     )
 
-@Client.on_callback_query(filters.regex('view'))
+@Client.on_callback_query(filters.regex("view"))
 @nice_errors
 async def view_callback(_, callback_query: CallbackQuery):
-    """ Handle the 'view' button press in a game. If the user is the host, send the game word as an alert. Otherwise, send a message indicating that the button is not for them. """
+    """Handle the 'view' button press in a game. If the user is the host, send the game word as an alert. Otherwise, send a message indicating that the button is not for them."""
     game = get_game(callback_query)
-    if game['host'].id == callback_query.from_user.id:
-        await callback_query.answer(game['word'], show_alert=True)
+    if game["host"].id == callback_query.from_user.id:
+        await callback_query.answer(game["word"], show_alert=True)
     else:
-        await callback_query.answer('This is not for you.', show_alert=True)
+        await callback_query.answer("This is not for you.", show_alert=True)
 
-@Client.on_callback_query(filters.regex('next'))
+@Client.on_callback_query(filters.regex("next"))
 @nice_errors
 async def next_callback(_, callback_query: CallbackQuery):
-    """ Handle the 'next' button press in a game. If the user is the host, send the next word as an alert. Otherwise, send a message indicating that the button is not for them. """
+    """Handle the 'next' button press in a game. If the user is the host, send the next word as an alert. Otherwise, send a message indicating that the button is not for them."""
     game = get_game(callback_query)
-    if game['host'].id == callback_query.from_user.id:
+    if game["host"].id == callback_query.from_user.id:
         next_word_result = await next_word(callback_query)
         await callback_query.answer(next_word_result, show_alert=True)
     else:
-        await callback_query.answer('This is not for you.', show_alert=True)
+        await callback_query.answer("This is not for you.", show_alert=True)
 
-@Client.on_message(filters.text & ~filters.command & filters.chat_type.filters.SUPERGROUP)
+@Client.on_message(filters.text & ~filters.command & filters.chat_type(ChatType.SUPERGROUP))
 @nice_errors
 async def guess_callback(_, message: Message):
-    """ Handle user guesses in a game. If the user guesses the correct word, update the database and send a reply with an inline keyboard. """
+    """Handle user guesses in a game. If the user guesses the correct word, update the database and send a reply with an inline keyboard."""
     try:
         game = get_game(message)
-        if game['host'].id != message.from_user.id:
+        if game["host"].id != message.from_user.id:
             if await is_true(message.text, message):
                 await message.reply_text(
                     f"{message.from_user.mention_html()} guessed the correct word, {game['word']}.",
                     reply_markup=inline_keyboard_markup,
                 )
-                # Add try-except block for database update
                 try:
                     await users.update(
                         message.chat.id,
@@ -117,17 +115,14 @@ async def guess_callback(_, message: Message):
 @Client.on_message(filters.command("abort"))
 @nice_errors
 async def abort_callback(_, message: Message):
-    """ Handle the '/abort' command. Abort the current game. """
+    """Handle the '/abort' command. Abort the current game."""
     try:
         await end_game(message)
         await message.reply_text(
-            f'{message.from_user.mention_html()} aborted the game.',
+            f"{message.from_user.mention_html()} aborted the game.",
             reply_markup=inline_keyboard_markup,
         )
-        # Add try-except block for database update
         try:
             await chats.update(message.chat.id, message.chat.title)
         except Exception as e:
-            logging.error(f"Error updating chat title: {e}")
-    except Exception as e:
-        logging.error(f"Error handling abort command: {e}")
+            logging.error(f"Error handling abort command: {e}")
