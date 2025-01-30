ff89ff7b3b1a946806e3e5aa33b2dca1978e9f20
Itssaravanakrish <77931259+Itssaravanakrish@users.noreply.github.com>
2025-01-30 14:21:40 +0530

    Update cmnds.py

--- plugins/cmnds.py
+++ plugins/cmnds.py
@@ -53,7 +53,7 @@ async def scores_callback(_, message: Message):
         parse_mode="HTML",
     )
 
-@Client.on_message(filters.command("start") & filters.group)
+@app.on_message(filters.command("start") & filters.group)
 @nice_errors
 async def start_callback(_, message: Message):
     """Handle the '/start' command in a group chat. Start a new game and update the database with the chat details."""
