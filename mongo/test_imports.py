cea65a9a7966165743f41d68e72f0ef262e613c4
Itssaravanakrish <77931259+Itssaravanakrish@users.noreply.github.com>
2025-01-31 19:44:34 +0530

    Create test_imports.py

---
+++ mongo/test_imports.py
@@ -0,0 +1,10 @@
+# test_imports.py
+
+from mongo.mongo import MongoDB
+from mongo.users_and_chats import Users, Chats
+
+def main():
+    print("Imports successful!")
+
+if __name__ == "__main__":
+    main()
