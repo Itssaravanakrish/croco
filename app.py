b9e0dad5c5c8b0cf12c29667f9250409c0caf6c7
Itssaravanakrish <77931259+Itssaravanakrish@users.noreply.github.com>
2025-02-14 22:26:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from flask import Flask
+from flask import Flask, request
+from main import bot  # Import bot after app
+from aiohttp import web
+import asyncio
+from plugins.web_support import web_server # Import web_server
 
 app = Flask(__name__)
 
@@ -6,10 +10,27 @@ app = Flask(__name__)
 def hello_world():
     return "TamilBots"
 
-# Import bot and webhook_handler AFTER app is defined.
-from main import bot
-from plugins.web_support import webhook_handler # Import webhook_handler
-
 @app.route("/your_webhook_path", methods=["POST"])  # Your webhook route
+async def webhook_handler():
+    update = request.get_json()
+    if update:
+        await bot.process_new_updates(update)
+    return "ok"
+
+
 async def handle_webhook(): # Call the imported webhook handler
     return await webhook_handler() # Await the coroutine
+
+
+async def start_server(): # Start the aiohttp server
+    web_app = await web_server()  # Get the aiohttp web app
+    app_runner = web.AppRunner(web_app)
+    await app_runner.setup()
+    bind_address = "0.0.0.0"
+    await web.TCPSite(app_runner, bind_address, int(os.environ.get("PORT", 8080))).start()
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.create_task(bot.start())
+    loop.run_until_complete(start_server())
