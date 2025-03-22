8a6ba1785c6a062733fbec28e9755707f7473bf7
Itssaravanakrish <77931259+Itssaravanakrish@users.noreply.github.com>
2025-03-22 16:31:50 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -91,12 +91,12 @@ async def handle_start_command(client, message, is_group: bool):
 
 # Command handler for /start in private messages
 @Client.on_message(filters.command("start") & filters.private)
-async def start_private(client, message):
+async def start_private(client: Client, message: Message):
     await handle_start_command(client, message, is_group=False)
 
 # Command handler for /start in group messages
 @Client.on_message(filters.command("start") & filters.group)
-async def start_group(client, message):
+async def start_group(client: Client, message: Message):
     await handle_start_command(client, message, is_group=True)
     
 @Client.on_callback_query()
